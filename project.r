42cc813c1428c92080fb46e900153bcfe8777a02
Eros <eros.fabrici@gmail.com>
2020-01-21 14:32:06 +0100

    initial commit

---
+++ project.r
@@ -0,0 +1,7 @@
+leaf <- read.csv("leaf/leaf.csv", header = FALSE,
+                 col.names = c("Class", "Speciment_n°", "Eccentricity", "Aspect_Ratio",
+                               "Elongation", "Solidity", "Stochastic_Convexity",
+                               "Isoperimetric_Factor", "Maximal_Indentation_Depth",
+                               "Lobedness", "Average_Intensity", "Average_Contrast",
+                               "Smoothness", " Third_Moment", "Uniformity",
+                               "Entropy"))
