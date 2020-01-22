d84b17f93b8a9bbc6db662fd56753a70a09592f6
Eros <eros.fabrici@gmail.com>
2020-01-22 18:23:11 +0100

    keep experimenting with rf

--- project.r
+++ project.r
@@ -4,6 +4,9 @@ library(rpart.plot)
 #colored plots (use function fancyRpartPlot())
 library(RColorBrewer)
 library(rattle)
+library(caret)
+library(e1071)
+library(caTools)
 leaf <- read.csv("leaf.csv", header = FALSE,
                  col.names = c("Class", "Speciment_n°", "Eccentricity", "Aspect_Ratio",
                                "Elongation", "Solidity", "Stochastic_Convexity",
@@ -12,6 +15,21 @@ leaf <- read.csv("leaf.csv", header = FALSE,
                                "Smoothness", " Third_Moment", "Uniformity",
                                "Entropy"))
 leaf$Class <- as.factor(leaf$Class)
+set.seed(3000)
+spl <- sample.split(leaf$Class, SplitRatio = 0.7)
+Train <- subset(leaf, spl == TRUE)
+Test <- subset(leaf, spl == FALSE)
+numFolds <- trainControl(method = "cv", number = 4)
+cpGrid <- expand.grid(.cp = seq(0.01, 0.5, 0.01))
+formula <- Class ~.
+train(formula, data = leaf, method = "rpart", trControl = numFolds, tuneGrid = cpGrid)
+
+treeCV <- rpart(formula, data = Train, cp = 0.02, method = "class")
+predictionTreeCV <- predict(treeCV, newdata = Test, type = "class")
+confMatrixTreeCV <- table(Test$Class, predict)
+print(paste("Accuracy: ", sum(diag(confMatrixTreeCV))/sum(confMatrixTreeCV)))
+#---------------------------------------------------------------------------------------------
+
 set.seed(1)
 K <- 10
 #check if there are some NA values
@@ -20,15 +38,15 @@ shuffled <- leaf[sample(nrow(leaf)),]
 
 accuracy <- rep(0,10)
 
-for (i in 1:K) {
+for (i in 1:4) {
   # These indices indicate the interval of the test set
-  indices <- (((i-1) * round((1/20)*nrow(shuffled))) + 1):((i*round((1/20) * nrow(shuffled))))
+  indices <- (((i-1) * round((1/4)*nrow(shuffled))) + 1):((i*round((1/4) * nrow(shuffled))))
   #take all the rows execpt those between 1:indices
   train <- shuffled[-indices,]
   #take all the rows with indices = 1:indices
   test <- shuffled[indices,]
   
-  tree <- rpart(Class ~ ., train, method="class", minbucket=10)
+  tree <- rpart(Class ~ ., train, method="class")
   pred <- predict(tree, test,type="class")
   confusionM <- table(test$Class,pred)
   accuracy[i] <- sum(diag(confusionM))/sum(confusionM)
--- randomForest.R
+++ randomForest.R
@@ -1,4 +1,117 @@
 library(randomForest)
+library(caret)
+library(mlbench)
+set.seed(3000)
+spl <- sample.split(leaf$Class, SplitRatio = 0.7)
+Train <- subset(leaf, spl == TRUE)
+Test <- subset(leaf, spl == FALSE)
+
+
+rf <- rfcv(Train[,-1], Train$Class, cv.fold = 5)
+with(rf, plot(n.var, error.cv, type="b", col="red"))
+r <- randomForest(Class~., data=Train, mtry=2)
+p <- predict(r, Test)
+cf <- confusionMatrix(p, Test$Class)
+# Create model with default paramters
+control <- trainControl(method="repeatedcv", number=4, repeats=3)
+seed <- 7
+metric <- "Accuracy"
+set.seed(seed)
+mtry <- sqrt(ncol(leaf)-1)
+tunegrid <- expand.grid(.mtry=mtry)
+rf_default <- train(Class~., data=Train, method="rf", metric=metric,
+                    tuneGrid=tunegrid, trControl=control)
+print(rf_default)
+
+# Random Search
+control <- trainControl(method="repeatedcv", number=4, repeats=3, search="random")
+set.seed(seed)
+mtry <- sqrt(ncol(leaf)-1)
+rf_random <- train(Class~., data=Train, method="rf", 
+                   metric=metric, tuneLength=15, trControl=control)
+print(rf_random)
+plot(rf_random)
+
+#Grid search
+control <- trainControl(method="repeatedcv", number=10, repeats=1, search="grid")
+set.seed(seed)
+tunegrid <- expand.grid(.mtry=c(1:15))
+rf_gridsearch <- train(Class~., data=Train, method="rf", metric=metric,
+                       tuneGrid=tunegrid, trControl=control)
+print(rf_gridsearch)
+plot(rf_gridsearch)
+# mtry = 7 seems to be optimal 0.7734
+
+
+#custom RF
+customRF <- list(type = "Classification", library = "randomForest", loop = NULL)
+customRF$parameters <- data.frame(parameter = c("ntree", "mtry"), class = rep("numeric", 2),
+                                  label = c("ntree", "mtry"))
+customRF$grid <- function(x, y, len = NULL, search = "grid") {}
+customRF$fit <- function(x, y, wts, param, lev, last, weights, classProbs, ...) {
+  randomForest(x, y, ntree=param$ntree, mtry=param$mtry, ...)
+}
+customRF$predict <- function(modelFit, newdata, preProc = NULL, submodels = NULL)
+  predict(modelFit, newdata)
+customRF$prob <- function(modelFit, newdata, preProc = NULL, submodels = NULL)
+  predict(modelFit, newdata, type = "prob")
+customRF$sort <- function(x) x[order(x[,1]),]
+customRF$levels <- function(x) x$classes
+
+# train model-----------------------------------------------------------------------
+K <- 5
+accuracy=rep(0,K)
+best_mtry=rep(0,K)
+best_ntree=rep(0,K)
+n <- nrow(shuffled)
+for(i in 1:K) {
+  indexes <- ((i-1)*round(1/K*n) + 1):i*round(1/K*n)
+  print(paste("i=", i, "i_min=", (i-1)*round(1/K*n) + 1, " I_max=", i*round(1/K*n)))
+  rm(train_custom)
+  rm(test_custom)
+  train_custom <- shuffled[-indices,] 
+  test_custom <- shuffled[indices,]
+  print(nrow(train_custom) + nrow(test_custom) == n)
+  control <- trainControl(method="cv", number=4)
+  tunegrid <- expand.grid(.ntree=c(1000, 1500, 2000, 2500), .mtry=c(1:15))
+  custom <- train(Class~., data=train_custom, method=customRF, metric="Accuracy",
+                  tuneGrid=tunegrid, trControl=control)
+  best_mtry[i] <- custom$bestTune$mtry
+  best_ntree[i] <- custom$bestTune$ntree
+  randomF <- randomForest(Class~., data = train_custom, mtry = best_mtry[i],
+                          ntree = best_ntree[i])
+  prediction <- predict(randomF, test_custom)
+  confMat <- table(test_custom$Class, prediction)
+  accuracy[i] <- sum(diag(confMat))/sum(confMat)
+}
+print(accuracy)
+print(best_mtry)
+print(best_ntree)
+#---------------------------------------------------------------------------------
+rf_CV <- randomForest(Class~., data = Train)
+predict_RFCV <- predict(rf_CV, Test)
+confMatrixRFCV <- table(Test$Class, predict_RFCV)
+print(paste("Accuracy: ", sum(diag(confMatrixRFCV))/sum(confMatrixRFCV)))
+
+
+rf_CV <- randomForest(Class~., data = Train, mtry=2)
+predict_RFCV <- predict(rf_CV, Test)
+confMatrixRFCV <- table(Test$Class, predict_RFCV)
+print(paste("Accuracy: ", sum(diag(confMatrixRFCV))/sum(confMatrixRFCV)))
+#acc = 0.8452
+
+rf_CV <- randomForest(Class~., data = Train, mtry=7, ntree=2500)
+predict_RFCV <- predict(rf_CV, Test)
+confMatrixRFCV <- table(Test$Class, predict_RFCV)
+print(paste("Accuracy: ", sum(diag(confMatrixRFCV))/sum(confMatrixRFCV)))
+#acc=0.80
+
+rf_CV <- randomForest(Class~., data = Train, mtry=14, ntree=2500)
+predict_RFCV <- predict(rf_CV, Test)
+confMatrixRFCV <- table(Test$Class, predict_RFCV)
+print(paste("Accuracy: ", sum(diag(confMatrixRFCV))/sum(confMatrixRFCV)))
+# acc = 0.82
+#------------------------------------------------------------------------------------------
 n <- nrow(leaf)
 nTrees <- c(1,10,25,50,75,100,250,500,750,1000,1250,1500,1750,2000,2250,2500,2750,3000)
 errRates <- c()
@@ -11,7 +124,7 @@ for(numbOfTrees in nTrees) {
   accuracy <- rep(0,10)
   #Cross Validation
   for (i in 1:K) {
-    indexes <- ((i-1)*round(1/10*n) + 1):i*round(1/10*n)
+    indexes <- ((i-1)*round(1/K*n) + 1):i*round(1/K*n)
     train <- shuffled[-indexes,]
     test <- shuffled[indexes,]
     #train
